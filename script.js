183f5ce7d8723ebb6c254059b8446c8ca88cc4a8
developervijaysingh <133250658+developervijaysingh@users.noreply.github.com>
2023-11-12 10:32:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("myForm").addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    // You can add your form submission logic here
+    
+    alert("Form submitted!"); // Replace this with your logic
+});
